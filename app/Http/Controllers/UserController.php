3d8374e72a685864ff7531e688b6c1fefcf13564
Armando <arzival.sistemas@gmail.com>
2024-10-20 14:13:15 -0600

    Update: crando el archivo de rutas api y declarando su uso en el app.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string',
+            'secret_word' => 'required|string',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'secret-word' => $request->secret_word,
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created user!',
+        ], 201);
+    }
+}
