784d0b9b46a7c9564f9153333bf0a9cec6f77b0e
Armando <arzival.sistemas@gmail.com>
2024-11-03 18:21:48 -0600

    Update: Creando funcionas necesarias para el registro, para el login y para guardar la palabra secreta

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,14 +14,12 @@ class UserController extends Controller
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
             'password' => 'required|string',
-            'secret_word' => 'required|string',
         ]);
 
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            'secret_word' => bcrypt($request->secret_word),
         ]);
 
         $user->save();
@@ -28,5 +27,44 @@ class UserController extends Controller
         return response()->json([
             'message' => 'Successfully created user!',
         ], 201);
+    }
+    
+    public function registerSecretWord(Request $request)
+    {
+        $request->validate([
+            'secret_word' => 'required|string',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $user = User::find($request->user_id);
+        $user->secret_word = bcrypt($request->secret_word);
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created secret word!',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
     }
 }
